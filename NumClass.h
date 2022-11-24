a59d3543da77ac67768fe73e83f296c62bc64e50
barmud <118991774+barmud@users.noreply.github.com>
2022-11-24 19:22:33 +0200

    Add files via upload

---
+++ NumClass.h
@@ -0,0 +1,14 @@
+/*will return if a number is Armstrong number
+An ArmStrong numbre is a n-digit number that is equal to the sum of the powers of its digits.
+For Example:407 = 43 + 03 + 73 = 64 + 0 + 343 = 407
+*/
+int isArmStrong(int);
+/*will return if a number is a palindrome*/
+int isPalindrome(int);
+/*will return if a number is a prime*/
+int isPrime(int);
+/*Strong numbrer is a special number whose sum of the factorial of digits is equal to the original
+number. For Example:145 is strong number
+1! + 4! + 5! = 145
+*/
+int isStrong(int);
